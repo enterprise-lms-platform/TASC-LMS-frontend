711e3e3347c693b62ad3a3e24c5928649c18c3fc
Patricia <pirmerpatricia99@gmail.com>
2026-01-13 14:04:55 +0300

    modified: src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LandingPage from "./pages/LandingPage";
 import RegistrationPage from "./pages/RegistrationPage";
 import LoginPage from "./pages/LoginPage";
+import VerificationPage from './pages/VerificationPage';
 
 const App = () => {
   return (
@@ -10,6 +11,8 @@ const App = () => {
         <Route path="/" element={<LandingPage />} />
         <Route path="/register" element={<RegistrationPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/verify" element={<VerificationPage />} />
+        
       </Routes>
     </BrowserRouter>
   )
