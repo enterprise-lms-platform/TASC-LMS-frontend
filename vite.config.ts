d7584c8e79a317b502b36e742ecf5d6aced6b0c9
Peter Kakuru Bingwa <pkakuru@gmail.com>
2026-03-24 08:29:17 +0300

    fix(build): bundle all vendor dependencies into a single chunk

--- vite.config.ts
+++ vite.config.ts
@@ -12,4 +12,13 @@ export default defineConfig({
       },
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) return 'vendor'
+        },
+      },
+    },
+  },
 })
